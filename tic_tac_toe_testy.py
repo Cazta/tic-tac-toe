d70925515812a14b6886aba7321c86fc467f79c3
“Christian <--rename-all>
2023-04-19 12:04:17 +0200

    update 19.04.

--- tic_tac_toe_testy.py
+++ tic_tac_toe_testy.py
@@ -1,12 +1,13 @@
 #Define global variables
 game_board = ['~','~','~','~','~','~','~','~','~']
 
-player_1 = None
-player_2 = None
+global player_1
+global player_2
 
-game_active = True
+global game_active
 
 active_player = None
+
 
 
 #start function
@@ -14,8 +15,20 @@ def start_function():
      print('How about a round of Tic-Tac-Toe?')
      start = input('Hit any key to start.')
      if start != None:
-        game_active == True
+        game_active = True
+              
 
+#assigning symbols to player
+def symbol_func():
+    print("Welcome to a new round of Tic Tac Toe")
+    #global player_1 
+    active_player = (input("Player 1 Please choose your Symbol, X or O: "))
+    #global player_2 
+    if active_player == "X" or active_player == "x":
+        player_2 = "O" 
+    else:
+        player_2 = "X"
+    print(f'Player 1 chose "{active_player}" and Player 2 chose "{player_2}"')
 
 #print board game
 def print_game_board(game_board):
@@ -25,34 +38,14 @@ def print_game_board(game_board):
     print('---------------------------')
     print(' | ' + '  ' + game_board[6] + '  ' + ' | ' + '  ' + game_board[7] + '  ' + ' | ' + '  ' + game_board[8] + '  ' + ' | ' )
 
-
-#assigning symbols to player
-def symbol_func(print):
-    print("Welcome to a new round of Tic Tac Toe")
-    player_1 = (input("Player 1 Please choose your Symbol, X or O: "))
-    if player_1 == "X" or player_1 == "x":
-        player_2 = "O" 
-    else:
-        player_2 = "X"
-    print(f'Player 1 chose "{player_1}" and Player 2 chose "{player_2}"')
-    #print(f'Player 1 chose "{player_1}" so Player 2 becomes "{player_2}"')
-
-
-#Declaring active player
+'''  #Declaring active player
 def declare_active_player(player_1):
-    active_player = player_1
-    print(active_player)
-
-
-#Switch Active Player
-def switch_active_player():
-    active_player = player_2
-    print(active_player)
-
+    #global active_player
+    active_player = player_1'''
 
 #move player
 def move_player(active_player):
-    idx = int(input(f'{active_player}, Please input value 1 - 9 for the corresponding field in the game board:'))
+    idx = int(input(f'{active_player}, please input value 1 - 9 for the corresponding field in game board:'))
     idx = idx-1
     game_board[idx] = active_player
     print_game_board(game_board)
@@ -62,6 +55,7 @@ def move_player(active_player):
 def check_win_horizontal(game_board, active_player):
     if game_board[0] == game_board[1] == game_board[2] == active_player or game_board[3] == game_board[4] == game_board[5] == active_player or game_board[6] == game_board[7] == game_board[8] == active_player:
         print(f'{active_player} won!')
+        #global game_active
         game_active = False
     else:
         game_active = True
@@ -71,15 +65,17 @@ def check_win_horizontal(game_board, active_player):
 def check_win_vertical(game_board, active_player):
     if game_board[0] == game_board[3] == game_board[6] == active_player or game_board[1] == game_board[4] == game_board[7] == active_player or game_board[2] == game_board[5] == game_board[8] == active_player:
         print(f'{active_player} won!')
+        #global game_active
         game_active = False
     else:
         game_active = True
 
 
 #check win diagonal
-def check_win_vertical(game_board, active_player):
+def check_win_diagonal(game_board, active_player):
     if game_board[0] == game_board[4] == game_board[8] == active_player or game_board[2] == game_board[4] == game_board[6] == active_player:
         print(f'{active_player} won!')
+        #global game_active
         game_active = False
     else:
         game_active = True
@@ -87,26 +83,45 @@ def check_win_vertical(game_board, active_player):
 
 #check draw
 def check_draw(game_board):
-    for element in game_board:
-        if '~' not in game_board:
-            print("It's a Draw!")
-            game_active = False
-        else:
-            game_active = True
+    #global game_active
+    if '~' not in game_board:
+        game_active = False
+    else:
+        game_active = True
+    if game_active == False:
+        print("It's a Draw!")
+
+
+
+#Switch Active Player
+def switch_active_player():
+    #global active_player
+    if active_player == player_1:
+        active_player = player_2
+    elif active_player == player_2:
+        active_player = player_1
+
 
 
 #while loop to start all functions
-#while game_active:
-if __name__ == "__main__":
+def play_game():
     start_function()
-    print_game_board(game_board)
-    symbol_func(print)
-    declare_active_player(player_1)
-    switch_active_player()
-    move_player(active_player)
-    check_win_horizontal(game_board, active_player)
-    check_win_vertical(game_board, active_player)
-    check_draw(game_board)
+    symbol_func()
+    #declare_active_player(player_1)
+    game_active = True
+    while game_active == True:
+        move_player(active_player)
+        check_win_horizontal(game_board, active_player)
+        check_win_vertical(game_board, active_player)
+        check_win_diagonal(game_board, active_player)
+        check_draw(game_board)
+        switch_active_player()
+    else:
+        print("The game is over!")
+        
+
+play_game()
+  
 
     
     
